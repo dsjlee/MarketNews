9f56138dc194b73479d37be695e6a618f047d582
dsjlee <>
2020-05-23 14:57:49 -0700

    Add project files.

---
+++ WasmServer/Shared/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WasmServer.Shared
+{
+    public class Article
+    {
+        public string Category { get; set; }
+        public double DateTime { get; set; }
+        public string Headline { get; set; }
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Related { get; set; }
+        public string Source { get; set; }
+        public string Summary { get; set; }
+        public string Url { get; set; }
+
+        public DateTime Date => new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(DateTime).ToLocalTime();
+    }
+}
